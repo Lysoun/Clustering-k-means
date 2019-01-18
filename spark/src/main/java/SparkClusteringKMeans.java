3a4be07a8b125c00d6f39898d8b466ce6e175cff
Élise RUBIO <rubio.elise@gmail.com>
2019-01-18 16:33:46 +0100

    jpp

--- spark/src/main/java/SparkClusteringKMeans.java
+++ spark/src/main/java/SparkClusteringKMeans.java
@@ -5,8 +5,6 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import org.apache.spark.SparkConf;
 
-import scala.Tuple2;
-
 import java.util.Arrays;
 
 public class SparkClusteringKMeans {
@@ -22,9 +20,9 @@ public class SparkClusteringKMeans {
         // Load the text into a Spark RDD, which is a distributed representation of each line of text
         JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
         JavaPairRDD<String, Integer> counts = textFile
-                .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
-                .mapToPair(word -> new Tuple2<>(word, 1))
-                .reduceByKey((a, b) -> a + b);
+	    .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
+	    .mapToPair(word -> (word, 1))
+	    .reduceByKey((a, b) -> a + b);
         counts.foreach(p -> System.out.println(p));
         System.out.println("Total words: " + counts.count());
         counts.saveAsTextFile("/tmp/shakespeareWordCount");
